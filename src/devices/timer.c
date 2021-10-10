20296160006fe1771ac1715a9d489ad04af8c126
linhan <linhan@shanghaitech.edu.cn>
2021-10-10 12:38:46 +0000

    oldlevel

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -92,8 +92,9 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  // while (timer_elapsed (start) < ticks) 
+    // thread_yield ();
+  enum intr_level old_level = intr_disable ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
