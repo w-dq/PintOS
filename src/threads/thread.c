be888bb51cba22f4fa45eda291e0d850a040460d
wudq1 <wudq1@shanghaitech.edu.cn>
2021-10-12 06:45:23 +0000

    alarm-priority 8/27

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -237,7 +237,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, priority_is_less, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -308,7 +308,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, priority_is_less, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -329,6 +329,12 @@ thread_foreach (thread_action_func *func, void *aux)
       struct thread *t = list_entry (e, struct thread, allelem);
       func (t, aux);
     }
+}
+// list_entry(LIST_ELEM, STRUCT, MEMBER)
+/* compare the priority of threads  return true if a > b*/
+bool
+priority_is_less(const struct list_elem *a,const struct list_elem *b, void *aux){
+    return list_entry(a, struct thread, elem)->priority > list_entry(b, struct thread, elem)->priority;
 }
 
 /* Sets the current thread's priority to NEW_PRIORITY. */
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -133,6 +133,7 @@ void thread_yield (void);
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
 
+bool priority_is_less (const struct list_elem *,const struct list_elem *, void *);
 int thread_get_priority (void);
 void thread_set_priority (int);
 
