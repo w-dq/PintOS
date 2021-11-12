f9c4ea11b756b93f05732ed2a44b12af39ed0a8c
linhan <linhan@shanghaitech.edu.cn>
2021-11-12 02:53:54 +0000

    del sema_up in start_process

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -89,7 +89,8 @@ start_process (void *file_name_)
   if (!success){
     palloc_free_page (file_name);
     
-    sema_up(&(thread_current()->parent->sema_wait)); //? parent?
+    // sema_up(&(thread_current()->parent->sema_wait)); //? parent?
+    
     thread_exit ();
   }
 
